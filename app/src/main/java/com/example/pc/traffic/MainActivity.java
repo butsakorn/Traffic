72afd6c26670942693a47797df4f996e63a425be
mww <butt.mw@gmail.com>
2016-09-28 17:16:43 +0700

    เพิ่มDetailแล้ว

--- app/src/main/java/com/example/pc/traffic/MainActivity.java
+++ app/src/main/java/com/example/pc/traffic/MainActivity.java
@@ -39,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
+        
     }   //createListView
     private void myIntentToDetail(int intClick){
         Intent objIntent = new Intent(MainActivity.this, ShowDetailActivity.class);
